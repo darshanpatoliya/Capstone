dd0cb0c3c119d3adf72252e8ea06b7b6ca974d21
Varshil0010 <Vpatel2996@conestogac.on.ca>
2023-02-04 21:41:52 -0500

    Project created

---
+++ projectMERN/clientfrontend/src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./../components/Header";
+import ShopSection from "./../components/homeComponents/ShopSection";
+import ContactInfo from "./../components/homeComponents/ContactInfo";
+import CalltoActionSection from "./../components/homeComponents/CalltoActionSection";
+import Footer from "./../components/Footer";
+import Slider from "../components/Slider";
+
+const HomeScreen = ({ match }) => {
+  window.scrollTo(0, 0);
+  const keyword = match.params.keyword;
+  const pagenumber = match.params.pagenumber;
+  return (
+    <div>
+      <Header />
+      <Slider/>
+      <ShopSection keyword={keyword} pagenumber={pagenumber} />
+      <CalltoActionSection />
+      <ContactInfo />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomeScreen;
