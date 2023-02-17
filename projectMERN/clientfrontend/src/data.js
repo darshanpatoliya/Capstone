5ded44807fb7988711f9dafb18158df264da1011
Varshil0010 <Vpatel2996@conestogac.on.ca>
2023-02-17 15:22:44 -0500

    slider change

--- projectMERN/clientfrontend/src/data.js
+++ projectMERN/clientfrontend/src/data.js
@@ -1,11 +1,12 @@
 import echo from './images/echo.jpg'
 import echo1 from './images/echo1.png'
 import headphone from './images/headphone.jpg'
+import desk from './images/desktop.jpg'
 
 export const sliderItems = [
     {
         id: 1,
-        image: echo,
+        image: desk,
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON TECHNOLOGY! GET FREE SHIPPING ON ALL ITEM",
         bg: "f5fafd"
