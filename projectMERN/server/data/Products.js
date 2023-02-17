79de97a1141c6e785cb82a431a5df98499c1bd28
darshanpatoliya <darshanpatoliya05@gmail.com>
2023-02-16 21:47:11 -0500

    All description added to products

--- projectMERN/server/data/Products.js
+++ projectMERN/server/data/Products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    name: "Webcam Camera",
+    name: "Logitech C920x HD Pro Webcam, Full HD 1080p/30fps Video Calling, Clear Stereo Audio, HD Light Correction - Black",
     image: "/images/6.png",
     description:
       "High resolution 1080P Full HD Webcam. This 1080P webcam supply Crisp Image and Crystal Clear Video. Webcam captures your clips in true 1080p glory, and 30 frames per second, so even the most detailed makeup tutorials look crisp and clear",
@@ -10,7 +10,7 @@ const products = [
     numReviews: 4,
   },
   {
-    name: "RGB Mousepad",
+    name: "RAGZAN RGB Gaming Mouse Pad Large,LED XXL Extended Mouse Mat Thick and Foldable Mat Anti-Fray Stitched Edges",
     image: "/images/5.png",
     description:
       "Brighter RGB light barThe RGB gaming mouse pad has a brand-new 3.0 upgraded luminous strip , beautiful colors, higher brightness, longer luminous life,creating a brighter RGB lighting effects that provides you with a better gaming and working atmosphere experience",
@@ -20,7 +20,7 @@ const products = [
     numReviews: 2,
   },
   {
-    name: "Keyboard(full size layout)",
+    name: "Apple: Magic Keyboard with Touch ID for Mac Computers with Apple Silicon - US English",
     image: "/images/4.png",
     description:
       "AESTHETIC & FUNCTIONAL: With a 104 key full sized layout, built-in numeric keypad, and scissor switch keycaps, combined with a sleek low profile body, this keyboard for mac perfectly integrates style and practicality for your workstation",
@@ -30,7 +30,7 @@ const products = [
     numReviews: 3,
   },
   {
-    name: "Light weight mouse",
+    name: "Logitech G PRO X SUPERLIGHT Wireless Gaming Mouse, Ultra-Lightweight, HERO 25K Sensor, 25,600 DPI",
     image: "/images/3.png",
     description:
       "Micro-precise scrolling: New designed-for-web scrolling makes searches, shopping and browsing easier^Feel-good design: Contoured shape and textured rubber grips keep your hand comfortable even after long hours^Long battery life: Up to 18 months of battery life virtually eliminates the need to change batteries.",
@@ -40,21 +40,31 @@ const products = [
     numReviews: 9,
   },
   {
-    name: "Curved Monitor",
+    name: "Dell 32 Curved Gaming Monitor",
     image: "/images/2.png",
     description:
-      "Featuring an ultra-slim and sleek profile the Samsung CF390 monitor measures less than 0.5inch thick. Make a stylish statement while staying productive with the 24-inch curved screen. The simple circular stand will add a modern look to your space.",
+      "Featuring an ultra-slim and sleek profile the Dell CF390 monitor measures less than 0.5inch thick. Make a stylish statement while staying productive with the 32-inch curved screen. The simple circular stand will add a modern look to your space.",
     price: 149,
     countInStock: 7,
     rating: 2,
     numReviews: 2,
   },
   {
-    name: "HeadPhone (Black)",
+    name: "JBL Live 460NC(Wireless on-ear NC headphones)",
     image: "/images/1.png",
     description:
       "Enjoy more of the detail in all your favourite songs thanks to 30mm dome drivers. Take and make calls without ever touching your phone. With a high-quality built-in microphone, you will be able to have hands-free conversations on your smartphone with ease.",
     price: 89,
+    countInStock: 20,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Amazon: All-New Echo Dot (5th Gen, 2022 release) with clock | Smart speaker with clock and Alexa | Cloud Blue",
+    image: "/images/echo.jpg",
+    description:
+      "Enjoy an improved audio experience compared to any previous Echo Dot with Alexa for clearer vocals, deeper bass and vibrant sound in any room.",
+    price: 85,
     countInStock: 20,
     rating: 0,
     numReviews: 0,
